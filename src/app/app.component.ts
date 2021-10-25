4668b43167b7452d0bca8b2a0d3b5e83c36abc92
Nimantha Aravinda <2018cs015@stu.ucsc.cmb.ac.lk>
2021-10-26 00:18:47 +0530

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-project';
+export class AppComponent implements OnInit {
+  appTitle = 'Welcome to App';
+  myString = "I Love Me";
+  myBoolean = false;
+
+  alertMe(val: any){
+    alert(val);
+  }
+  constructor(){
+    
+  }
+  ngOnInit() {
+    
+  }
 }
